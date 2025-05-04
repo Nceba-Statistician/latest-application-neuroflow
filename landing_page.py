b603deb1756c1ab8363f07e561ac1b3445a34759
Nceba <143310721+Nceba-Statistician@users.noreply.github.com>
2025-05-05 01:07:06 +0200

    Update landing_page.py

--- landing_page.py
+++ landing_page.py
@@ -3,5 +3,5 @@ import streamlit, pandas
 streamlit.write("Nceba Gagaza")
 streamlit.info("Profile coming soon!")
 streamlit.write("Education history, work experience, projects, etc")
-if streamlit.download_button("Download CV", data="", file_name="CV.txt"):
+if streamlit.download_button("Download CV", data="Updated CV coming soon!", file_name="CV.pdf"):
     streamlit.write("To be added.")
