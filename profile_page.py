2199c10f604994d14f7335f3a65ab348e3cacb8c
Nceba-Developer <143310721+Nceba-Developer@users.noreply.github.com>
2025-05-06 12:54:56 +0200

    updated

--- profile_page.py
+++ profile_page.py
@@ -6,9 +6,9 @@ def create_pdf():
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("Arial", size=12)
-    pdf.cell(200, 10, txt="Nceba Gagaza", ln=True, align="Centre")
-    pdf.cell(200, 10, txt="Profile coming soon!", ln=True, align="Centre")
-    pdf.cell(200, 10, txt="Education history, work experience, specialization, etc", ln=True, align="Centre")
+    pdf.cell(200, 10, txt="Nceba Gagaza", ln=True, align="C")
+    pdf.cell(200, 10, txt="Profile coming soon!", ln=True, align="C")
+    pdf.cell(200, 10, txt="Education history, work experience, specialization, etc", ln=True, align="C")
     return pdf.output(dest="S").encode("latin-1")
 
 streamlit.write("Nceba Gagaza")
